e6c058839ec2aefa56a9ce3352045eed507c58ad
debadrita17 <debadritadas2002@gmail.com>
2025-07-19 21:11:10 +0530

    add asignment2.py for task1 and task2 python files

---
+++ task1.py
@@ -0,0 +1,8 @@
+#assignment2
+#task1
+#perform to check a number is even or odd by using if-else and takes an integer as input.
+num=int(input('enter a number: '))
+if num%2==0:
+    print(num,'is an even number.')
+else:
+    print(num, 'is an odd number.')
---
+++ task2.py
@@ -0,0 +1,12 @@
+#assignment2
+#task2
+#perform to calculate the sum of integers from 1 to 50 by using for loop
+num=range(1,51)
+total =0
+for i in num:
+    total+=i
+print('the final sum of numbers from 1 to 50 is :',total)
+
+
+
+
